2b77883c6cb90c3c53e8f62443780441a1c7037c
juntao-dev <juntao.dev@gmail.com>
2021-02-18 21:47:43 +1100

    backup s3

--- README.md
+++ README.md
@@ -4,20 +4,40 @@ Set up AWS CLI
 
 Install Terraform
 
-To start, run
+### Fill in the following details
+
+1.  S3 bucket name in `main.tf`
+
+```
+locals {
+  minecraft_backup_s3_bucket_name = ""
+}
+```
+
+2. SSH Public Key in `script.sh`
+
+```sh
+echo "" >> /home/ec2-user/.ssh/authorized_keys
+```
 
 ```sh
 terraform init
 terraform apply
 ```
 
+---
+
 When deployed, the server IP would be shown. Connect via <IP>:25565
 (It would take a few minutes for the server to be ready)
 
 Terraform would create a bunch of files in the local dir to maintain state (do not touch or delete them!)
 
-When finished, run
+When finished, run the following command to destroy the stack (replace `<s3_bucket_name>`)
+
+If an error shows up saying that the bucket is not empty, run both commands again.
 
 ```sh
+aws s3 rm --recursive s3://<s3_bucket_name>
+
 terraform destroy
 ```
--- main.tf
+++ main.tf
@@ -7,9 +7,92 @@ terraform {
   }
 }
 
+locals {
+  minecraft_backup_s3_bucket_name = ""
+}
+
 provider "aws" {
   profile = "default"
   region  = "ap-southeast-2"
+}
+
+resource "aws_iam_role" "iam_minecraft_bot_role" {
+  name = "iam_minecraft_bot_role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_policy" "iam_minecraft_bot_policy" {
+  name        = "iam_minecraft_bot_policy"
+  path        = "/"
+  description = "IAM policy for minecraft instance"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "s3:PutObject",
+        "s3:ListBucket",
+        "s3:GetObject"
+      ],
+      "Resource": [
+        "${aws_s3_bucket.minecraft_world_data_backup_bucket.arn}",
+        "${aws_s3_bucket.minecraft_world_data_backup_bucket.arn}/*"
+      ],
+      "Effect": "Allow"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "iam_minecraft_bot_policy_attachment" {
+  role       = aws_iam_role.iam_minecraft_bot_role.name
+  policy_arn = aws_iam_policy.iam_minecraft_bot_policy.arn
+}
+
+resource "aws_iam_instance_profile" "iam_minecraft_bot_instance_profile" {
+  name = "iam_minecraft_bot_instance_profile"
+  role = aws_iam_role.iam_minecraft_bot_role.name
+}
+
+resource "aws_s3_bucket" "minecraft_world_data_backup_bucket" {
+  bucket = local.minecraft_backup_s3_bucket_name
+  acl    = "private"
+
+  # Enable server-side encryption by default
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "bucket_public_block" {
+  bucket = aws_s3_bucket.minecraft_world_data_backup_bucket.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
 
 resource "aws_instance" "minecraft_server" {
@@ -19,7 +102,8 @@ resource "aws_instance" "minecraft_server" {
   security_groups = [
     aws_security_group.allow_minecraft.name
   ]
-  user_data = file("script.sh")
+  iam_instance_profile = aws_iam_instance_profile.iam_minecraft_bot_instance_profile.name
+  user_data            = templatefile("${path.module}/script.sh", { bucket_name = local.minecraft_backup_s3_bucket_name })
 
   tags = {
     Name = "Minecraft Server Instance"
@@ -67,4 +151,8 @@ resource "aws_security_group" "allow_minecraft" {
 
 output "minecraft_server_ip" {
   value = "${aws_instance.minecraft_server.public_ip}:25565"
+}
+
+output "minecraft_backup_s3_bucket" {
+  value = local.minecraft_backup_s3_bucket_name
 }
